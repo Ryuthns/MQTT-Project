dd6184992f65689507a91caa96f082a7592dee33
Blu` <87508144+Ryuthns@users.noreply.github.com>
2021-09-03 21:12:32 +0700

    Create pubtext.py

---
+++ pub/pubtext.py
@@ -0,0 +1,11 @@
+# PUB_MES
+import paho.mqtt.client as mqtt
+
+# This is the Publisher
+
+client = mqtt.Client()
+client.connect("test.mosquitto.org",1883,60)
+for i in range(1000):
+    client.publish("thk0092", "Hello {}".format(i+1));
+
+client.disconnect();
