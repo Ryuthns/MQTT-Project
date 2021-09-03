b232579db5c0b6486bd74ba819d0c6fefbf73fd9
Blu` <87508144+Ryuthns@users.noreply.github.com>
2021-09-03 20:58:10 +0700

    Create subpic.py

---
+++ sub/subpic.py
@@ -0,0 +1,31 @@
+# SUB_PIC
+import paho.mqtt.client as mqtt
+
+cou = 1
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("kjpie1")
+  
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    global cou
+    name = "pic_sub/"+"receive"+str(cou)+".jpg"
+    f = open(name,'wb')
+    f.write(msg.payload)
+    f.close()
+    print ('image received {}'.format(cou))
+    cou +=1
+  
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("broker.hivemq.com", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
